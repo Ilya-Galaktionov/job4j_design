046bb2057f05f1080be87b0c832099453e815832
Ilya Galaktionov <igalaktionov1@gmail.com>
2024-09-02 22:36:29 +0300

    4. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +14,19 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>();
+        User user1 = new User("Ivan", 2, new GregorianCalendar(1988, 5, 17));
+        User user2 = new User("Ivan", 2, new GregorianCalendar(1988, 5, 17));
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
+        /**
+         * 5.1 Пары попали в разные бакеты.
+         * 5.2 Проверка ключей на равенство их хеш-кодов не проводилась, т.к. ключи попали в разные бакеты.
+         * 5.3 ПРоверка на equals() не проводилась, т.к. ключи попали в разные бакеты.
+         */
     }
 }
